e63a27cfcef82938437604a64d4de8eada8e167b
Vinay Chaudhary <ch.vinay2063@gmail.com>
2018-04-03 20:37:53 +0530

    Architecture of frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import logo from './logo.svg';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
